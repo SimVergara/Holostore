6e015c893a7e3b48a799639ceb04fe6501bbc99d
Simon Vergara <simon.vergara.1993@gmail.com>
2018-12-12 02:13:34 -0500

    changed how the grid is displayed. using bootstrap grid now

--- client/src/Inventory.js
+++ client/src/Inventory.js
@@ -4,61 +4,39 @@ import './Inventory.css';
 
 function Hologram(props){
   return(
-      <td style={{width:'25%'}}  className="InvRow">
-        <p>{props.id || "ID"}</p>
-        <h2>{props.name || "NAME"}</h2>
-        <p>{props.description || "description"}</p>
-      </td>
+    <div class={props.colsize}>
+      <h3 >{props.id || "ID"}</h3>
+      <h1>{props.name || "NAME"}</h1>
+      <h2>{props.description || "description"}</h2>
+    </div>
     );
 }
 
 class Inventory extends Component {
 
-  renderRow(rowItems){
-    let holos = [];
-    let numItems = rowItems.length;
-
-    for (let i=0; i < numItems; i++){
-      holos.push(
-        <Hologram 
-          key={i} 
-          name={rowItems[i].name} 
-          description={rowItems[i].description}
-          id={rowItems[i].id}
-        />);
-    }
-    return(holos);
-  }
-
   renderGrid(props){
-    let numItems = props.inventory.length;
-    let numRows = Math.floor(numItems/4);
-    let lastRow = numItems%4;
-    let rows = [];
-
-    for(var i=0; i<numRows; i++){
-      rows.push(
-        <tr key={i}>
-          {this.renderRow(props.inventory.slice((i*4),(i*4+4)))}
-        </tr>);
-    };
-    
-    rows.push(<tr key={i+1}>{this.renderRow(props.inventory.slice(numItems - lastRow))}</tr>);
-    
+    console.log(props);
     return(
-      <tbody > 
-          {rows}
-      </tbody>
+      props.inventory.map(function(item, i){
+        console.log('test');
+        return(
+          <Hologram 
+            key={i}
+            id={item.id}
+            name={item.name}
+            description={item.description}
+            colsize="col-sm-3"
+          />);
+      })
     );
   }
 
   render(){
     return(
       <div>
-        <p>{this.props.resp}</p>
-        <table>
+        <div class="row">
           {this.renderGrid(this.props)}
-        </table>
+        </div>
       </div>
     );
   }
